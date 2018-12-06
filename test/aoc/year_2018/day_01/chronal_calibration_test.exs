0c288a94702b2254ce5d519c35b509a0b1b1ce51
alistairstead <alistair_stead@me.com>
2018-12-06 08:30:08 +0000

    Day 1 part two complete

--- test/aoc/year_2018/day_01/chronal_calibration_test.exs
+++ test/aoc/year_2018/day_01/chronal_calibration_test.exs
@@ -16,4 +16,18 @@ defmodule Aoc.Year2018.Day01.ChronalCalibrationTest do
       assert input |> ChronalCalibration.part_1() == 561
     end
   end
+
+  describe "part_2/1" do
+    test "examples" do
+      assert ChronalCalibration.part_2("+1 -1") == 0
+      assert ChronalCalibration.part_2("+3 +3 +4 -2 -4") == 10
+      assert ChronalCalibration.part_2("-6 +3 +8 +5 -6") == 5
+      assert ChronalCalibration.part_2("+7 +7 -2 -7 -4") == 14
+    end
+
+    @tag day: 01, year: 2018
+    test "input", %{input: input} do
+      assert input |> ChronalCalibration.part_2() == 563
+    end
+  end
 end
